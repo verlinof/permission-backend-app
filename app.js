37a6b3b248d3fb71b012cb93bf1ced67df15d031
verlinof <ajikoko.zx@gmail.com>
2024-03-23 14:32:54 +0700

    Feat: Permission.*

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const app = express();
 //Import Router
 const userRoute = require('./routes/userRoute');
 const verificatorRoute = require('./routes/verificatorRoute');
+const adminRoute = require('./routes/adminRoute');
+const permissionRoute = require('./routes/permissionRoute');
+// const perm
 
 app.use(bodyParser.json());
 
 //Routes
 app.use('/auth', userRoute);
 app.use('/verificator', verificatorRoute);
-
+app.use('/admin', adminRoute);
+app.use('/permission', permissionRoute);
 
 module.exports = app
